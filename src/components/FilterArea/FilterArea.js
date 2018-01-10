4254a7a3e1ba38adcbb69a83bcded3925d40d865
Dakota Lillie <dakota.lillie@icloud.com>
2018-01-10 17:28:47 -0500

    finish search bar styling

--- src/components/FilterArea/FilterArea.js
+++ src/components/FilterArea/FilterArea.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Row } from 'react-bootstrap';
+import { Grid, Row } from 'react-bootstrap';
 import SearchBar from '../SearchBar/SearchBar';
 
 const FilterArea = () => {
   return (
     <div className="filter_area">
-      <Row>
-        <SearchBar />
-      </Row>
-      <Row>
-        <h1>testing</h1>
-      </Row>
+      <Grid>
+        <Row>
+          <SearchBar />
+        </Row>
+      </Grid>
     </div>
   );
 };
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Button, FormGroup, FormControl, Glyphicon } from 'react-bootstrap';
+import './SearchBar.css';
 
 class SearchBar extends React.Component {
+  state = {
+    value: ''
+  };
+
+  handleChange = e => {
+    this.setState({ value: e.target.value });
+  };
+
+  handleClick = e => {
+    this.setState({ value: '' });
+  };
+
   render = () => {
     return (
       <div className="search_bar">
-        <h1>Hello from SearchBar</h1>
+        <FormGroup bsSize="large">
+          <FormControl
+            type="text"
+            value={this.state.value}
+            placeholder="Search"
+            onChange={this.handleChange}
+            className="main_search_input"
+          />
+          {this.state.value ? (
+            <Button className="clear_search_button" onClick={this.handleClick}>
+              <Glyphicon glyph="remove" />
+            </Button>
+          ) : null}
+        </FormGroup>
       </div>
     );
   };
