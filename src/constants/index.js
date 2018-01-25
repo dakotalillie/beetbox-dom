f48d4498a7250ad5ea632605bce2605bc51d3747
Dakota Lillie <dakota.lillie@icloud.com>
2018-01-25 14:58:34 -0500

    adjust root references

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const API_ROOT = `http://127.0.0.1:3000/api/v1`;
-export const API_WS_ROOT = `ws://127.0.0.1:3000/api/v1/cable`;
+export const API_ROOT = `https://beetbox-api.herokuapp.com/api/v1`;
+export const API_WS_ROOT = `ws://beetbox-api.herokuapp.com/api/v1/cable`;
 export const token = localStorage.getItem('token');
 export const headers = {
   'Content-Type': 'application/json',
